6e4d4ce3ee740ae4b1701ace3f3f1042ca008999
bittcrak <bittcrak@mysterybox.stolen>
2026-01-30 21:28:12 +0530

    new theme, obsidian

--- sway/scripts/powermenu.sh
+++ sway/scripts/powermenu.sh
@@ -6,9 +6,9 @@
 # Theme: Catppuccin
 
 
-options="󰌾 Lock\n󰍃 Logout\n󰑓 Reboot\n PowerOff"
+options="󰌾 Lock\n󰍃 Logout\n󰑓 Reboot\n PowerOff\nZz Sleep"
 
-choice=$(echo -e $options| wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+choice=$(echo -e $options| wofi --width 250 --height 250 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 if [[ "$choice" == "lock" ]]; then
     swaylock -c 000000
@@ -22,6 +22,8 @@ elif [[ "$choice" == "reboot" ]]; then
 elif [[ "$choice" == "poweroff" ]];then
     systemctl poweroff
 
+elif [[ "$choice" == "sleep" ]];then
+	systemctl sleep
 else
     echo
 fi
